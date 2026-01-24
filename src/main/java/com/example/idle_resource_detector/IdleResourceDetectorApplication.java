6578c3a0624ba1e94afe33763027fc27cd349e38
chaitrali1742000 <chaitralimirashi@gmail.com>
2026-01-24 11:00:18 +0530

    Initial Commit

---
+++ src/main/java/com/example/idle_resource_detector/IdleResourceDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.idle_resource_detector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdleResourceDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdleResourceDetectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/idle_resource_detector/IdleResourceDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.idle_resource_detector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdleResourceDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
